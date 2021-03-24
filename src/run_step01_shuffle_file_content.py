a1b98fc3d47ea4a1b71084b696b2753a6c64b3e7
M. Dimitrova <milena.bd@gmail.com>
2021-03-24 10:31:02 +0200

    Cleanup

--- src/run_step01_shuffle_file_content.py
+++ src/run_step01_shuffle_file_content.py
@@ -5,14 +5,12 @@ from src.helpers.shuffle_content_helper import shuffle_files_content
 
 # Add Logger
 add_logger(file_name='01_shuffle_data.log')
-logging.warning("!!! This step takes about 120 min to complete !!!")
+logging.warning("!!! This step takes about 150 min to complete !!!")
 
 source_files_dir = 'E:\\machine-learning\\datasets\\iot23\\2_attacks\\'
 source_files_pattern = source_files_dir + "*.csv"
 
 shuffle_files_content(source_files_pattern)
-
-# shuffle_file_in_partitions('E:\\machine-learning\\datasets\\iot23\\2_attacks\\DDoS.csv', partitions_per_gb=1)
 
 print('The end')
 quit()
