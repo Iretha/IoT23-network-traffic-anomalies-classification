ac090971554738d3bbd72e664e1c4d7eae9eef33
M. Dimitrova <milena.bd@gmail.com>
2021-03-25 16:22:59 +0200

    Add some flexibility when running pre-made examples

--- src/helpers/experiments_helper.py
+++ src/helpers/experiments_helper.py
@@ -12,7 +12,9 @@ def run_experiments(data_dir,
                     features_selections,
                     training_algorithms,
                     test_size=0.2,
-                    overwrite=False):
+                    overwrite=False,
+                    enable_exp_data_preparation=True,
+                    enable_model_training=True):
     for data_sample in data_samples:
         for feature_selection in features_selections:
             __run_experiment(data_dir,
@@ -21,41 +23,48 @@ def run_experiments(data_dir,
                              feature_selection,
                              training_algorithms,
                              test_size=test_size,
-                             overwrite=overwrite)
+                             overwrite=overwrite,
+                             enable_exp_data_preparation=enable_exp_data_preparation,
+                             enable_model_training=enable_model_training)
 
 
 def __run_experiment(data_dir,
                      experiments_dir,
                      data_sample,
-                     feature_combination,
+                     feature_selection,
                      training_algorithms,
                      test_size=0.2,
-                     overwrite=False):
+                     overwrite=False,
+                     enable_exp_data_preparation=True,
+                     enable_model_training=True):
     # Make experiment data dir
-    experiment_name = get_exp_name(data_sample, feature_combination)
+    experiment_name = get_exp_name(data_sample, feature_selection['description'])
     experiment_data_dir = experiments_dir + get_exp_data_dir(experiment_name)
     mk_dir(experiment_data_dir)
 
-    # Prepare experiment data
     data_file_name = data_sample['clean_data_file_name']
-    split_into_train_and_test(data_dir,
-                              data_file_name,
-                              experiment_data_dir,
-                              test_size=test_size,
-                              features=feature_combination['features'],
-                              overwrite=overwrite)
-    logging.info("****** Experiment data is in " + experiment_data_dir)
+
+    # Prepare experiment data
+    if enable_exp_data_preparation:
+        split_into_train_and_test(data_dir,
+                                  data_file_name,
+                                  experiment_data_dir,
+                                  test_size=test_size,
+                                  features=feature_selection['features'],
+                                  overwrite=overwrite)
+        logging.info("****** Experiment data is in " + experiment_data_dir)
 
     # Make experiment models dir
     experiment_models_dir = experiments_dir + get_exp_models_dir(experiment_name)
     mk_dir(experiment_models_dir)
 
     # Train models
-    classification_col = data_cleanup["classification_col"]
-    train_data_file_path = experiment_data_dir + get_train_data_path(data_file_name)
-    create_models(experiment_models_dir,
-                  train_data_file_path,
-                  classification_col,
-                  training_algorithms,
-                  overwrite=overwrite)
-    logging.info("****** Experiment models are in " + experiment_models_dir)
+    if enable_model_training:
+        classification_col = data_cleanup["classification_col"]
+        train_data_file_path = experiment_data_dir + get_train_data_path(data_file_name)
+        create_models(experiment_models_dir,
+                      train_data_file_path,
+                      classification_col,
+                      training_algorithms,
+                      overwrite=overwrite)
+        logging.info("****** Experiment models are in " + experiment_models_dir)
--- src/helpers/process_helper.py
+++ src/helpers/process_helper.py
@@ -13,48 +13,68 @@ def run_end_to_end_process(source_files_dir,
                            data_samples,
                            features,
                            training_algorithms,
-                           report_file_name):
+                           overwrite=False,
+                           enable_data_preprocessing=True,
+                           enable_clean_data_charts=True,
+                           enable_train_data_charts=True,
+                           enable_experiment_data_preparation=True,
+                           enable_model_training=True,
+                           enable_score_tables=True,
+                           enable_score_charts=True,
+                           plot_corr=True,
+                           plot_cls_dist=True,
+                           plot_attr_dist=True,
+                           enable_model_insights=True,
+                           enable_final_report=True,
+                           final_report_name='all_scores.xlsx'):
     file_header = iot23_metadata["file_header"]
 
     # Prepare Data
-    run_data_preprocessing(source_files_dir,
-                           data_dir,
-                           file_header,
-                           data_cleanup,
-                           data_samples=data_samples,
-                           overwrite=False)
+    if enable_data_preprocessing:
+        run_data_preprocessing(source_files_dir,
+                               data_dir,
+                               file_header,
+                               data_cleanup,
+                               data_samples=data_samples,
+                               overwrite=overwrite)
 
     # Explore Data
-    explore_clean_data(data_dir,
-                       data_samples=data_samples,
-                       plot_corr=True,
-                       plot_cls_dist=True,
-                       plot_attr_dist=True)
+    if enable_clean_data_charts:
+        explore_clean_data(data_dir,
+                           data_samples=data_samples,
+                           plot_corr=plot_corr,
+                           plot_cls_dist=plot_cls_dist,
+                           plot_attr_dist=plot_attr_dist)
 
     # Run Experiments
-    run_experiments(data_dir,
-                    experiments_dir,
-                    data_samples,
-                    features,
-                    training_algorithms,
-                    overwrite=False)
+    if enable_experiment_data_preparation or enable_model_training:
+        run_experiments(data_dir,
+                        experiments_dir,
+                        data_samples,
+                        features,
+                        training_algorithms,
+                        overwrite=overwrite,
+                        enable_exp_data_preparation=enable_experiment_data_preparation,
+                        enable_model_training=enable_model_training)
 
     # Explore Train/ Test Data
-    explore_experiments_train_test_data(experiments_dir,
-                                        data_samples,
-                                        features,
-                                        plot_corr=True,
-                                        plot_cls_dist=True,
-                                        plot_attr_dist=True)
+    if enable_train_data_charts:
+        explore_experiments_train_test_data(experiments_dir,
+                                            data_samples,
+                                            features,
+                                            plot_corr=plot_corr,
+                                            plot_cls_dist=plot_cls_dist,
+                                            plot_attr_dist=plot_attr_dist)
 
     # Explore Experiments Output
     run_experiments_reports(experiments_dir,
                             data_samples,
                             features,
-                            enable_score_tables=True,
-                            enable_score_charts=True,
-                            enable_model_insights=True)
+                            enable_score_tables=enable_score_tables,
+                            enable_score_charts=enable_score_charts,
+                            enable_model_insights=enable_model_insights)
 
     # Combine all stats into single XLS
-    experiments_folder_list = list_folder_names(experiments_dir)
-    combine_reports(experiments_dir, experiments_folder_list, report_file_name)
+    if enable_final_report:
+        experiments_folder_list = list_folder_names(experiments_dir)
+        combine_reports(experiments_dir, experiments_folder_list, final_report_name)
